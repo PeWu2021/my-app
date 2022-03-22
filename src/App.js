afc265bc16f1f1c5c12c7e554989df23eaea63c1
PeWu2021 <pwasilczuk@gmail.com>
2022-03-22 16:37:56 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [year, setYear] = useState();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        fetch(`http://localhost:3000/date?year=${year}`)
+            .then(res => res.json())
+            .then(date => console.log(date))
+    }
+    return (
+        <>
+        <div className="form-container">
+
+            <form className="form" onSubmit={handleSubmit}>
+                <h1 className="form__title">Podaj datę</h1>
+                <select value={year} onChange={e => setYear(e.target.value)}>
+                    <option value={2021}>2021</option>
+                    <option value={1914}>1914</option>
+                    <option value={2022}>2022</option>
+
+                </select>
+                <input type="text" className="form__input" placeholder="Podaj rok" required  />
+                <input type="text" className="form__input" placeholder="Podaj miesiąc" required />
+                <input type="submit" className="form__button" value="Znajdź" />
+            </form>
+        </div>
+            <div className="result"/>
+
+            <footer className="footer">
+                <p className="footer_p">Informacje zebrane na podstawie <a href="https://pl.wikipedia.org">pl.wikipedia.org</a></p></footer>
+            </>
+);
 }
 
 export default App;
---
+++ src/date.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const date = [
+    {
+        id: 1,
+        year: 1914,
+        month: "sierpień",
+        day: 4,
+        happening: "I wojna światowa: Kalisz został zbombardowany przez Niemców",
+
+    },
+    {
+        id: 2,
+        year: 1914,
+        month: "październik",
+        day: 9,
+        happening: " I wojna światowa: napad Niemców na tereny Królestwa Polskiego",
+    }
+]
+
+
